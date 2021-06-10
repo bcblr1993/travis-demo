0cbe171c55bacbed893303d2076fe471819672bd
xiaochen <chenynqc@163.com>
2021-06-10 15:55:45 +0800

    ci: 更新 .travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 ssh root@8.140.185.101 'mkdir -p /root/travis-demo'
-scp ./target/travis-demo-0.0.1-SNAPSHOT.jar root:@8.140.185.101:/root/travis-demo
+scp ./target/travis-demo-0.0.1-SNAPSHOT.jar root@8.140.185.101:/root/travis-demo
 ssh root@8.140.185.101 'cd /root/travis-demo && java -jar travis-demo-0.0.1-SNAPSHOT.jar'
